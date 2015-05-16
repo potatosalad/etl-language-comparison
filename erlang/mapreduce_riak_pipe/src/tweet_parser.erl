2fd20dd80314b7508ceafe376f17ed2faab6900c
Andrew Bennett <potatosaladx@gmail.com>
2015-05-15 19:21:51 -0600

    Use binary:match/2,3 and binary:part/3 for performance improvements.

--- erlang/mapreduce_riak_pipe/src/tweet_parser.erl
+++ erlang/mapreduce_riak_pipe/src/tweet_parser.erl
@@ -2,60 +2,49 @@
 
 -export([parse/2]).
 
-parse(<<>>, _SendOutput) ->
-	ok;
+-define(SEARCH, [
+	<<"knicks">>,<<"Knicks">>,<<"kNicks">>,<<"KNicks">>,
+	<<"knIcks">>,<<"KnIcks">>,<<"kNIcks">>,<<"KNIcks">>,
+	<<"kniCks">>,<<"KniCks">>,<<"kNiCks">>,<<"KNiCks">>,
+	<<"knICks">>,<<"KnICks">>,<<"kNICks">>,<<"KNICks">>,
+	<<"knicKs">>,<<"KnicKs">>,<<"kNicKs">>,<<"KNicKs">>,
+	<<"knIcKs">>,<<"KnIcKs">>,<<"kNIcKs">>,<<"KNIcKs">>,
+	<<"kniCKs">>,<<"KniCKs">>,<<"kNiCKs">>,<<"KNiCKs">>,
+	<<"knICKs">>,<<"KnICKs">>,<<"kNICKs">>,<<"KNICKs">>,
+	<<"knickS">>,<<"KnickS">>,<<"kNickS">>,<<"KNickS">>,
+	<<"knIckS">>,<<"KnIckS">>,<<"kNIckS">>,<<"KNIckS">>,
+	<<"kniCkS">>,<<"KniCkS">>,<<"kNiCkS">>,<<"KNiCkS">>,
+	<<"knICkS">>,<<"KnICkS">>,<<"kNICkS">>,<<"KNICkS">>,
+	<<"knicKS">>,<<"KnicKS">>,<<"kNicKS">>,<<"KNicKS">>,
+	<<"knIcKS">>,<<"KnIcKS">>,<<"kNIcKS">>,<<"KNIcKS">>,
+	<<"kniCKS">>,<<"KniCKS">>,<<"kNiCKS">>,<<"KNiCKS">>,
+	<<"knICKS">>,<<"KnICKS">>,<<"kNICKS">>,<<"KNICKS">>
+]).
+
 parse(Binary, SendOutput) ->
-	parse_tweet(Binary, SendOutput).
+	parse(Binary, binary:compile_pattern(<< $\n >>), binary:compile_pattern(<< $\t >>), binary:compile_pattern(?SEARCH), SendOutput).
 
-parse_tweet(<< $\t, Rest/binary >>, SendOutput) ->
-	parse_tweet_hood(Rest, <<>>, SendOutput);
-parse_tweet(<< _, Rest/binary >>, SendOutput) ->
-	parse_tweet(Rest, SendOutput);
-parse_tweet(<<>>, _SendOutput) ->
-	ok.
-
-parse_tweet_hood(<< $\t, Rest/binary >>, Hood, SendOutput) ->
-	parse_next_tab(Rest, Hood, SendOutput);
-parse_tweet_hood(<< $\n, Rest/binary >>, _Hood, SendOutput) ->
-	parse(Rest, SendOutput);
-parse_tweet_hood(<< C, Rest/binary >>, Hood, SendOutput) ->
-	parse_tweet_hood(Rest, << Hood/binary, C >>, SendOutput);
-parse_tweet_hood(<<>>, _Hood, _SendOutput) ->
-	ok.
-
-parse_next_tab(<< $\t, Rest/binary >>, Hood, SendOutput) ->
-	parse_tweet_message(Rest, Hood, SendOutput);
-parse_next_tab(<< $\n, Rest/binary >>, _Hood, SendOutput) ->
-	parse(Rest, SendOutput);
-parse_next_tab(<< _, Rest/binary >>, Hood, SendOutput) ->
-	parse_next_tab(Rest, Hood, SendOutput);
-parse_next_tab(<<>>, _Hood, _SendOutput) ->
-	ok.
-
-parse_tweet_message(<< K, Rest/binary >>, Hood, SendOutput)
-		when (K =:= $K orelse K =:= $k) ->
-	parse_tweet_message_match(Rest, Hood, SendOutput);
-parse_tweet_message(<< $\n, Rest/binary >>, _Hood, SendOutput) ->
-	parse(Rest, SendOutput);
-parse_tweet_message(<< _, Rest/binary >>, Hood, SendOutput) ->
-	parse_tweet_message(Rest, Hood, SendOutput);
-parse_tweet_message(<<>>, _Hood, _SendOutput) ->
-	ok.
-
-parse_tweet_message_match(<< N, I, C, K, S, Rest/binary >>, Hood, SendOutput)
-		when (N =:= $N orelse N =:= $n)
-		andalso (I =:= $I orelse I =:= $i)
-		andalso (C =:= $C orelse C =:= $c)
-		andalso (K =:= $K orelse K =:= $k)
-		andalso (S =:= $S orelse S =:= $s) ->
-	SendOutput({Hood, 1}),
-	parse_next_line(Rest, SendOutput);
-parse_tweet_message_match(Rest, Hood, SendOutput) ->
-	parse_tweet_message(Rest, Hood, SendOutput).
-
-parse_next_line(<< $\n, Rest/binary >>, SendOutput) ->
-	parse(Rest, SendOutput);
-parse_next_line(<< _, Rest/binary >>, SendOutput) ->
-	parse_next_line(Rest, SendOutput);
-parse_next_line(<<>>, _SendOutput) ->
-	ok.
+parse(Binary, Newline, Tab, Search, SendOutput) ->
+	case binary:match(Binary, Newline) of
+		{Pos, _} ->
+			case binary:match(Binary, Search, [{scope, {0, Pos}}]) of
+				{_, _} ->
+					case binary:match(Binary, Tab) of
+						{A, _} ->
+							case binary:match(Binary, Tab, [{scope, {A+1, Pos-A-1}}]) of
+								{B, _} ->
+									Hood = binary:part(Binary, A+1, B-A-1),
+									SendOutput({Hood, 1}),
+									parse(binary:part(Binary, Pos+1, byte_size(Binary)-Pos-1), Newline, Tab, Search, SendOutput);
+								_ ->
+									erlang:error(badarg)
+							end;
+						_ ->
+							erlang:error(badarg)
+					end;
+				_ ->
+					parse(binary:part(Binary, Pos+1, byte_size(Binary)-Pos-1), Newline, Tab, Search, SendOutput)
+			end;
+		_ ->
+			ok
+	end.
